6c981e12f438d080baebee0911f7f9aa1a25c381
Brian Worsham <brian.w.worsham@hotmail.com>
2023-08-07 13:29:46 -0500

    Add project files.

---
+++ Phase3Section2.20/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Phase3Section2._20.Models;
+
+namespace Phase3Section2._20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(StudentModel model)
+        {
+            StringBuilder sb = new StringBuilder("Form data:\n");
+            sb.Append(model.Name + ", " + model.Address + "," + model.Class + "," + model.Email);
+            return Content(sb.ToString());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
